72e20c214b0d97eddc154a90f3ef7a6f10ca9fc1
sorawat38 <st.sorawat38@gmail.com>
2024-10-05 16:37:06 -0600

    [feat]  add query package

--- controllers/packages.js
+++ controllers/packages.js
@@ -1,4 +1,18 @@
-function getPackages(req, res) {}
+const packageService = require("../services/package");
+
+/**
+ * Get a vacation packages
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ */
+async function getPackages(req, res) {
+  try {
+    const packages = await packageService.getAllPackages();
+    res.status(200).json(packages);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
 
 function postOrder(req, res) {}
 
---
+++ services/package.js
@@ -0,0 +1,25 @@
+const Package = require("../models/packages");
+
+/**
+ * Fetch all vacation packages from the database
+ * @returns {Promise<Array>} An array of vacation packages
+ */
+// 1. Each package should display a description, start and end dates, and price.
+// 2. Before including a package on the page, make sure that the package end date is greater than (or equal to) the current date
+// 3. Also, check whether the package start date is less than the current date, and if it is, write out some CSS to make the start date bold and red
+async function getAllPackages() {
+  try {
+    const packages = await Package.findAll();
+    let currentDateTime = new Date();
+    const filteredPackages = packages.filter(
+      (pkg) => pkg.PkgEndDate >= currentDateTime
+    );
+    return filteredPackages;
+  } catch (error) {
+    throw new Error("Error fetching packages: " + error.message);
+  }
+}
+
+module.exports = {
+  getAllPackages,
+};
