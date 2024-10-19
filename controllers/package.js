b8ee741a56a3548aa8eaf3e2a4f397451038c166
sorawat38 <st.sorawat38@gmail.com>
2024-10-18 23:11:02 -0600

    [feat] change request body of posting order

--- controllers/package.js
+++ controllers/package.js
@@ -1,5 +1,5 @@
 const packageService = require("../services/package");
-const { param, body, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const { ErrorResponse } = require("./error");
 
 /**
@@ -17,39 +17,41 @@ async function getPackagesHandler(req, res) {
 }
 
 const postOrderFields = [
-  "TravelerCount",
-  "TripTypeId",
-  "CustFirstName",
-  "CustLastName",
-  "CustAddress",
-  "CustCity",
-  "CustProv",
-  "CustPostal",
-  "CustCountry",
-  "CustHomePhone",
-  "CustBusPhone",
-  "CustEmail",
-  "AgentId",
+  "travelerCount",
+  "tripTypeId",
+  "packageId",
+  "custFirstName",
+  "custLastName",
+  "custAddress",
+  "custCity",
+  "custProv",
+  "custPostal",
+  "custCountry",
+  "custBusPhone",
+  "custEmail",
+  "userId",
+  "password",
 ];
 
 const validatePostOrder = [
   // Check body is empty
   body().notEmpty().withMessage("Request body cannot be empty"),
-  // Check param is empty
-  param("packageId").notEmpty().withMessage("packageId param is empty"),
   // Check all fields must not empty
   ...postOrderFields.map((field) =>
     body(field).notEmpty().withMessage(`${field} is required`)
   ),
-  param("packageId")
-    .isInt()
-    .withMessage("packageId param must be valid integer"),
-  body("CustEmail") // CustEmail
+  body("custEmail") // CustEmail
     .isEmail()
-    .withMessage("CustEmail must be a valid email address"),
-  body("CustHomePhone", "CustBusPhone") // CustHomePhone and CustBusPhone
+    .withMessage("custEmail must be a valid email address"),
+  body("custHomePhone")
+    .optional({ checkFalsy: true })
+    .trim()
     .isMobilePhone()
-    .withMessage("Phone must be a valid phone number"),
+    .withMessage("Home Phone must be a valid phone number"),
+  body("custBusPhone")
+    .isMobilePhone()
+    .withMessage("Businees Phone must be a valid phone number"),
+  body("agentId").optional(),
 ];
 
 async function postOrderHandler(req, res) {
@@ -61,19 +63,16 @@ async function postOrderHandler(req, res) {
       .json(new ErrorResponse(errors.array()[0].msg, errors.array()[0]));
   }
   try {
-    const order = await packageService.postOrder(
-      req.params["packageId"],
-      req.body
-    );
+    const order = await packageService.postOrder(req.body);
     res.status(201).json({
-      message: "Success",
+      message: "success",
       data: {
         customerId: order.CustomerId,
         bookingId: order.BookingId,
       },
     });
   } catch (error) {
-    res.status(400).json(new ErrorResponse(error.message));
+    res.status(500).json(new ErrorResponse(error.message));
   }
 }
 
